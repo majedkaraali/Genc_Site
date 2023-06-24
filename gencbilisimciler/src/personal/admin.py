95a51ac57fe42fe3d97da2cb7d3cf1ce3675c530
Majed <majedkaraali@gmail.com>
2023-06-24 13:29:27 +0300

    Uopload

---
+++ gencbilisimciler/src/personal/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from personal.models import Event
+admin.site.register(Event)
---
+++ gencbilisimciler/src/personal/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from personal.models import Event 
+class CreateEvent(forms.ModelForm):
+
+	class Meta:
+		model = Event
+		fields = ['title', 'image', 'location','time' ]
