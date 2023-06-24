6a17ab27ea441b83ef35c0d91f307532addf6b78
Majed <majedkaraali@gmail.com>
2023-06-24 20:29:33 +0300

    update

--- gencbilisimciler/src/mysite/urls.py
+++ gencbilisimciler/src/mysite/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path('team/',team_view,name='team'),
 
 
-   # path('register/', registration_view, name="register"),
+    path('register/', registration_view, name="register"),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
