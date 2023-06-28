1cd4e3a47c3f0ccf3fe307c35d0c56efbdab2efa
Majed <majedkaraali@gmail.com>
2023-06-28 16:58:53 +0300

    asd

--- gencbilisimciler/src/mysite/urls.py
+++ gencbilisimciler/src/mysite/urls.py
@@ -49,6 +49,7 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
 	path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
    # path('blogs/',all_posts_view,name='blogs'),
+   
     path('events/',all_events,name='events'),
     path('team/',team_view,name='team'),
 
