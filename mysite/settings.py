4bb24ec9f1bd0ef49db6e33e22e0412171b952e6
Majed <majedkaraali@gmail.com>
2023-08-24 15:14:10 +0300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,7 +102,7 @@ SECRET_KEY = 'YNJtLEDk4w1o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 if DEBUG:
     pass
