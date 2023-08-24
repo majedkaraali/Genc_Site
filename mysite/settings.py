c0103e66a3aa14223140dee80da77d9cf9d44e6e
Majed <majedkaraali@gmail.com>
2023-08-24 20:30:27 +0300

    dj_database_url fx

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import dj_database_url
 from pathlib import Path
 
 
@@ -44,7 +45,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
